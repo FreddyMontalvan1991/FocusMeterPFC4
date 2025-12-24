053b24c1ef859dd88dc1aa8aecca570557191b4e
Christian Mendieta <christian.mendieta.t@gmail.com>
2025-12-24 07:43:26 -0500

    .

--- App/app_servicio.py
+++ App/app_servicio.py
@@ -30,39 +30,3 @@ def frame_modelo_atencion():
         content=encoded_image.tobytes(),
         media_type="image/jpeg"
     )
-
-
-
-"""
-@app.get("/frame")
-def frame_modelo_atencion():
-    if modelo_atencion.ultimo_frame is None:
-        return None
-    return {"frame": modelo_atencion.ultimo_frame}
-
-
-
-
-
-
-
-from fastapi import FastAPI
-from servicio import modelo_atencion
-import cv2
-import threading
-
-app = FastAPI()
-
-threading.Thread(target=modelo_atencion.ejecutar_modelo, daemon=True).start()
-
-@app.get("/estimacion_atencion")
-def ultima_estimacion_atencion():
-    return {"estimacion_atencion": modelo_atencion.ultimo_nivel}
-
-@app.get("/frame")
-def frame_modelo_atencion():
-    if modelo_atencion.ultimo_frame is None:
-        return None
-    _, buffer = cv2.imencode(".jpg", modelo_atencion.ultimo_frame)
-    return buffer.tobytes()
-"""
--- App/servicio/modelo_atencion.py
+++ App/servicio/modelo_atencion.py
@@ -1,6 +1,6 @@
 import cv2
 from ultralytics import YOLO
-#import time
+
 
 ultimo_frame = None
 ultimo_nivel = 0.0
@@ -31,7 +31,6 @@ def ejecutar_modelo():
         atentos = 0
         total = len(boxes)
         print(f"\nTotal cajas:{total}, atentos: {atentos}")
-        #time.sleep(1)
 
         for box in boxes:
             x1, y1, x2, y2 = map(int, box.xyxy[0])
@@ -46,7 +45,6 @@ def ejecutar_modelo():
             else:
                 color = (0, 0, 255)
 
-            # ===== DIBUJAR CUADRO =====
             cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
             cv2.putText(
                 frame,
@@ -58,51 +56,8 @@ def ejecutar_modelo():
                 2
             )
 
-        # ===== NIVEL DE ATENCIÓN (MODELO) =====
         ultimo_nivel = atentos / total if total > 0 else 0
         print(f'\nNivel Atención: {ultimo_nivel}.')
 
 
-        # ===== MOSTRAR VIDEO =====
         ultimo_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-"""import cv2
-import time
-from ultralytics import YOLO
-
-MODEL_PATH = "servicio/modelo_atencion_preentrenado.pt"
-CAMERA_INDEX = 0
-
-model = YOLO(MODEL_PATH)
-class_names = model.names
-
-cap = cv2.VideoCapture(CAMERA_INDEX)
-
-ultimo_frame = None
-ultimo_nivel = 0.0
-
-def ejecutar_modelo():
-    global ultimo_frame, ultimo_nivel
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            time.sleep(0.1)
-            continue
-
-        results = model(frame, conf=0.5)
-        boxes = results[0].boxes
-
-        atentos = 0
-        total = len(boxes)
-        print("Total boxes:", total)
-        for box in boxes:
-            cls_id = int(box.cls[0])
-            etiqueta = class_names[cls_id].lower()
-            print("Etiqueta detectada:", etiqueta)
-            if etiqueta in ["atento", "attentive"]:
-                atentos += 1
-
-        ultimo_nivel = atentos / total if total > 0 else 0
-        ultimo_frame = frame
-        time.sleep(0.03)
-        print(f'\n\nNivel atencion: {ultimo_nivel}')
-"""
